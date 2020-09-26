e7d563d8b33d4836b6d72babb8d2f42b23738db1
Crowdbotics <team@crowdbotics.com>
2020-09-26 21:24:11 +0000

    Update models

--- backend/animal_care_support_20743/wsgi.py
+++ backend/animal_care_support_20743/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'animal_care_support_20743.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "animal_care_support_20743.settings")
 
 application = get_wsgi_application()
